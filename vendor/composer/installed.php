3db864279921ee3c4abe2f4492de0ec537f2d138
Markus Staab <maggus.staab@googlemail.com>
2023-03-22 16:01:57 +0100

    build optimized composer autoloader

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '673a2abfca369dd2f83b530746d1e0c34a5025f3',
+        'reference' => 'd319ca21a70037bbf1cdc25815447a11f5b41eae',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '673a2abfca369dd2f83b530746d1e0c34a5025f3',
+            'reference' => 'd319ca21a70037bbf1cdc25815447a11f5b41eae',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
