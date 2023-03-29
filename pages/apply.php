e445545fee51d1779c2454978f2ddefddc2ece19
Markus Staab <maggus.staab@googlemail.com>
2023-03-29 10:41:01 +0200

    describe the migration result in more detail

--- pages/apply.php
+++ pages/apply.php
@@ -12,17 +12,35 @@ if ($addon === '') {
     throw new rex_exception('Missing addon parameter');
 }
 
+$backToUseCaseUrl = rex_url::backendPage('rexfactor/use-case').'&addon='.rex_escape($addon, 'url');;
 $backToStartUrl = rex_url::backendPage('rexfactor');
 
 $result = RexFactor::runRexFactor($addon, $setList, $targetVersion, false);
 
-$html = '';
+$hint = '';
 $total = $result->getTotals();
 if ($total['changed_files'] > 0) {
-    $headline = 'Successfully migrated '. $total['changed_files'] .' files';
+    echo '
+    <h2>Successfully migrated '. $total['changed_files'] .' files</h2>
+    <ol>
+        <li>
+            At this point you should review and test the changed source-code.<br>
+            You may use rexstan to detect potential issues.
+        </li>
+        <li style="margin-top: 10px">
+            After making sure everything still works as expected commit the changes.
+        </li>
+        <li style="margin-top: 10px">
+            Finally you can go ahead with the next migration use-case:<br>
+            <a class="btn btn-info" href="'. $backToStartUrl .'">Start next migration</a>
+            <a class="btn btn-info" href="'. $backToUseCaseUrl .'">Select next use-case for "'.rex_escape($addon).'"</a>
+        </li>
+        </ol>
+    ';
 } else {
-    $headline = 'No changes';
+    echo '
+    <h2>No changes</h2>
+    <a class="btn btn-info" href="'. $backToStartUrl .'">Start next migration</a>
+    <a class="btn btn-info" href="'. $backToUseCaseUrl .'">Select next use-case for "'.rex_escape($addon).'"</a>
+    ';
 }
-
-echo '<h2>'. $headline .'</h2>';
-echo '<a class="btn btn-info" href="'. $backToStartUrl .'">Start next migration</a>';
