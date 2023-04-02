6880e9ddd1c136709908721bd622e3e166776f6e
Markus Staab <maggus.staab@googlemail.com>
2023-04-02 16:13:56 +0200

    redaxo code style: modernize_strpos per default

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -7,6 +7,9 @@ $finder = (new PhpCsFixer\Finder())
 
 return (new Redaxo\PhpCsFixerConfig\Config())
     ->setRules([
+        // redaxo core ships with polyfills, so we can always apply these rectors, see https://github.com/redaxo/php-cs-fixer-config/issues/5
+        'modernize_strpos' => true,
+        // personal preference - I don't like yoda style, see https://github.com/redaxo/php-cs-fixer-config/issues/4
         'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false]
     ])
     ->setFinder($finder)
