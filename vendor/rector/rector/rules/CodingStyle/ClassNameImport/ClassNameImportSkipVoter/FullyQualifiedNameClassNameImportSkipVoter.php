02066fcaa705d4473603741dfff86e8769d0d581
Markus Staab <maggus.staab@googlemail.com>
2023-11-16 17:26:04 +0100

    update vendor

--- vendor/rector/rector/rules/CodingStyle/ClassNameImport/ClassNameImportSkipVoter/FullyQualifiedNameClassNameImportSkipVoter.php
+++ vendor/rector/rector/rules/CodingStyle/ClassNameImport/ClassNameImportSkipVoter/FullyQualifiedNameClassNameImportSkipVoter.php
@@ -3,9 +3,11 @@
 declare (strict_types=1);
 namespace Rector\CodingStyle\ClassNameImport\ClassNameImportSkipVoter;
 
+use RectorPrefix202311\Nette\Utils\Strings;
 use PhpParser\Node;
 use Rector\CodingStyle\ClassNameImport\ShortNameResolver;
 use Rector\CodingStyle\Contract\ClassNameImport\ClassNameImportSkipVoterInterface;
+use Rector\Core\Configuration\RenamedClassesDataCollector;
 use Rector\Core\ValueObject\Application\File;
 use Rector\StaticTypeMapper\ValueObject\Type\FullyQualifiedObjectType;
 /**
@@ -24,21 +26,41 @@ final class FullyQualifiedNameClassNameImportSkipVoter implements ClassNameImpor
      * @var \Rector\CodingStyle\ClassNameImport\ShortNameResolver
      */
     private $shortNameResolver;
-    public function __construct(ShortNameResolver $shortNameResolver)
+    /**
+     * @readonly
+     * @var \Rector\Core\Configuration\RenamedClassesDataCollector
+     */
+    private $renamedClassesDataCollector;
+    public function __construct(ShortNameResolver $shortNameResolver, RenamedClassesDataCollector $renamedClassesDataCollector)
     {
         $this->shortNameResolver = $shortNameResolver;
+        $this->renamedClassesDataCollector = $renamedClassesDataCollector;
     }
     public function shouldSkip(File $file, FullyQualifiedObjectType $fullyQualifiedObjectType, Node $node) : bool
     {
         // "new X" or "X::static()"
         /** @var array<string, string> $shortNamesToFullyQualifiedNames */
         $shortNamesToFullyQualifiedNames = $this->shortNameResolver->resolveFromFile($file);
+        $fullyQualifiedObjectTypeShortName = $fullyQualifiedObjectType->getShortName();
+        $className = $fullyQualifiedObjectType->getClassName();
+        $removedUses = $this->renamedClassesDataCollector->getOldClasses();
         foreach ($shortNamesToFullyQualifiedNames as $shortName => $fullyQualifiedName) {
-            if ($fullyQualifiedObjectType->getShortName() !== $shortName) {
+            if ($fullyQualifiedObjectTypeShortName !== $shortName) {
+                $shortName = $this->cleanShortName($shortName);
+            }
+            if ($fullyQualifiedObjectTypeShortName !== $shortName) {
                 continue;
             }
-            return $fullyQualifiedObjectType->getClassName() !== $fullyQualifiedName;
+            $fullyQualifiedName = \ltrim($fullyQualifiedName, '\\');
+            if ($className === $fullyQualifiedName) {
+                return \false;
+            }
+            return !\in_array($fullyQualifiedName, $removedUses, \true);
         }
         return \false;
+    }
+    private function cleanShortName(string $shortName) : string
+    {
+        return \strncmp($shortName, '\\', \strlen('\\')) === 0 ? \ltrim((string) Strings::after($shortName, '\\', -1)) : $shortName;
     }
 }
